5272bbe9e7a3ab1ae043aeb7c12f478d33d3fbeb
thsowers <tyler@startrakstudios.com>
2019-02-15 17:37:10 -0500

    prelim STFT work

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use num::complex::Complex;
 use rustfft::FFTplanner;
 use std::fs::File;
 use std::io::BufReader;
+use stft::{STFT, WindowType};
 
 trait Signal {
     fn energy(self) -> f64;
@@ -71,6 +72,64 @@ fn find_spectral_peak(spectrum: Vec<Complex<f32>>, num_samples: usize) -> Option
     }
 }
 
+fn output_data(spectrum: Vec<Complex<f32>>, num_samples: usize) {
+    println!("Outputing some data");
+    let audio_data = spectrum
+        .iter()
+        .take(num_samples / 2) // FFT is symmetrical, ignore first half
+        .enumerate();
+    println!("...Done");
+    for x in audio_data {
+        println!("{:?}", x)
+    }
+}
+
+fn generate_spectrogram() {
+    // let's generate ten seconds of fake audio
+    let sample_rate: usize = 44100;
+    let seconds: usize = 10;
+    let sample_count = sample_rate * seconds;
+    let all_samples = (0..sample_count).map(|x| x as f64).collect::<Vec<f64>>();
+
+    // let's initialize our short-time fourier transform
+    let window_type: WindowType = WindowType::Hanning;
+    let window_size: usize = 1024;
+    let step_size: usize = 512;
+    let mut stft = STFT::new(window_type, window_size, step_size);
+
+    // we need a buffer to hold a computed column of the spectrogram
+    let mut spectrogram_column: Vec<f64> =
+        std::iter::repeat(0.).take(stft.output_size()).collect();
+
+    // iterate over all the samples in chunks of 3000 samples.
+    // in a real program you would probably read from something instead.
+    for some_samples in (&all_samples[..]).chunks(10000) {
+        // append the samples to the internal ringbuffer of the stft
+        stft.append_samples(some_samples);
+
+        // as long as there remain window_size samples in the internal
+        // ringbuffer of the stft
+        while stft.contains_enough_to_compute() {
+            // compute one column of the stft by
+            // taking the first window_size samples of the internal ringbuffer,
+            // multiplying them with the window,
+            // computing the fast fourier transform,
+            // taking half of the symetric complex outputs,
+            // computing the norm of the complex outputs and
+            // taking the log10
+            stft.compute_column(&mut spectrogram_column[..]);
+
+            // here's where you would do something with the
+            // spectrogram_column...
+            println!("{:?}", spectrogram_column);
+
+            // drop step_size samples from the internal ringbuffer of the stft
+            // making a step of size step_size
+            stft.move_to_next_column();
+        }
+    }
+}
+
 fn main() {
     let matches = App::new("Solstice")
         .version("0.1")
@@ -105,7 +164,9 @@ fn main() {
 
     if let Some(o) = matches.value_of("input") {
         let (spectrum, num_samples) = get_audio_data(o);
-        println!("{}", find_spectral_peak(spectrum, num_samples).unwrap())
+        //println!("{}", find_spectral_peak(spectrum, num_samples).unwrap());
+        generate_spectrogram();
+        //output_data(spectrum, num_samples);
     } else {
         println!("Please provide an audio file");
     }
